9607cc4e48d2ada6ad15c241a8d61b1eca423449
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-02-14 16:58:22 +0100

    add info of first 10 in list

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import { newPage } from "./utilities.js";
 export const leftArrow = document.querySelector(".material-symbols-outlined:nth-of-type(1)");
 export const rightArrow = document.querySelector(".material-symbols-outlined:nth-of-type(2)");
 let pageIndex = 1;
+export const baseURL = "https://swapi.dev/api";
 
-fetchCharacters(`https://swapi.dev/api/people?page=${pageIndex}`).then((characters) => {
+fetchCharacters(`${baseURL}/people?page=${pageIndex}`).then((characters) => {
   addCharactersToDom(characters);
 });
 
 rightArrow.addEventListener("click", (e) => newPage(e));
 leftArrow.addEventListener("click", (e) => newPage(e));
-
--- utilities.js
+++ utilities.js
@@ -1,7 +1,10 @@
 const sectionOne = document.querySelector(".names");
+const hide = document.querySelector(".hide");
+const details = document.querySelector(".details");
 const currentPageNumber = document.querySelector(".current-page");
 import { fetchCharacters } from "./script.js";
-import { leftArrow, rightArrow } from "./index.js";
+import { baseURL, leftArrow, rightArrow } from "./index.js";
+
 let pageIndex = 1;
 let pageNumber = 1;
 
@@ -10,23 +13,47 @@ export async function addCharactersToDom(characters) {
     .map((character, index) => {
       const characterItem = {
         name: `${character.name}`,
-        index: index,
       };
-      console.log(characterItem.index);
+      index = index + 1;
       return `
            <section class="name-row">
-             <h4 class="name" id="${characterItem.index}"> ${characterItem.name} </h4>
+             <h4 class="character" id="${index}"> ${characterItem.name} </h4>
            </section>
            `;
     })
     .join("");
   sectionOne.innerHTML = getCharactersToDom;
-  let name = document.querySelector(".name");
-  name.addEventListener("click", (e) => fetchOneCharacter(e));
+  let infoCharacters = document.querySelectorAll(".character");
+  infoCharacters.forEach((character) =>
+    character.addEventListener("click", (e) => fetchOneCharacter(e))
+  );
 }
 
+function fetchOneCharacter(e) {
+  hide.classList.remove("hide");
+  let index = e.target.id;
+  console.log(index);
+  fetchCharacters(`${baseURL}/people/${index}`).then((character) => {
+    addCharacterToDom(character);
+  });
+}
+
+export function addCharacterToDom(character) {
+  const characterInfoElements = /*html*/ `
+  <h3 class="character-info-name">${character.name}</h3>
+  <div class="character-info">Height: ${character.height}</div>
+  <div class="character-info">Mass: ${character.mass} kg</div>
+  <div class="character-info">Hair color: ${character.hair_color}</div>
+  <div class="character-info">Skin color: ${character.skin_color}</div>
+  <div class="character-info">Eye color: ${character.eye_color}</div>
+  <div class="character-info">Birth year: ${character.birth_year}</div>
+  <div class="character-info">Gender: ${character.gender}</div>
+  `;
+
+  details.innerHTML = characterInfoElements;
+}
 export function newPage(e) {
-  if (e.target === rightArrow) {
+  if (e.target === rightArrow && pageIndex <= 8) {
     pageIndex++;
     fetchCharacters(`https://swapi.dev/api/people?page=${pageIndex}`).then((characters) => {
       addCharactersToDom(characters);
@@ -45,17 +72,4 @@ export function newPage(e) {
       currentPageNumber.innerHTML = `${pageNumber} / 8 `;
     }
   }
-}
-
-
-
-function fetchOneCharacter(e) {
-   let index = e.target.getAttribute("id");
-  fetchCharacters(`https://swapi.dev/api/people/${index}`).then((character) => {
-    addCharacterToDom(character, index);
-  });
-}
-function addCharacterToDom(character, index) {
-  console.log(index);
-  console.log(character);
 }
